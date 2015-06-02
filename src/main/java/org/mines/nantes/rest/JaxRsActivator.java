2bf2b9f7a3616b5c735d1f84f3f045e587d24168
Florian Lassont <florian.lassont@gmail.com>
2015-06-02 20:30:45 +0200

    [WS] Activator for Jax-RS WS

---
+++ src/main/java/org/mines/nantes/rest/JaxRsActivator.java
@@ -0,0 +1,12 @@
+package org.mines.nantes.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Jax-RS activator (Java EE6 style)
+ */
+@ApplicationPath("/rest")
+public class JaxRsActivator extends Application {
+	// Body remains intentionally blank
+}
