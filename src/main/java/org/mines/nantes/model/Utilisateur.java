83f435c56427f43f5265d26f3a17a3f78f13ed43
Alex Gourbiliere <alex.gourbi@gmail.com>
2015-06-04 21:14:08 +0200

    Ajout attribut admin + Getters/Setters Utilisateur

--- src/main/java/org/mines/nantes/model/Utilisateur.java
+++ src/main/java/org/mines/nantes/model/Utilisateur.java
@@ -32,8 +32,66 @@ public class Utilisateur {
 	@NotBlank
 	private String password;
 
+	private boolean admin;
+
 	/** List of Roles this Utilisateur has been given access to */
 	@ElementCollection
 	@Enumerated(EnumType.STRING)
 	private Set<UtilisateurRole> roles;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+	public Set<UtilisateurRole> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<UtilisateurRole> roles) {
+		this.roles = roles;
+	}
 }
