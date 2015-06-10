d0fad9f88a4832f855928201ee07d206b9e38318
Alex Gourbiliere <alex.gourbi@gmail.com>
2015-06-10 14:17:16 +0200

    MAJ AffectationService POST et PUT

--- src/main/java/org/mines/nantes/rest/AffectationService.java
+++ src/main/java/org/mines/nantes/rest/AffectationService.java
@@ -4,13 +4,13 @@ import org.mines.nantes.dao.AffectationDAO;
 import org.mines.nantes.model.Affectation;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.Collection;
 
 /**
@@ -31,5 +31,39 @@ public class AffectationService {
     public Response getAllAffectation() {
         Collection<Affectation> listAffectation = affectationDAO.getAllAffectation();
         return Response.ok(listAffectation).build();
+    }
+
+    @POST
+    @Consumes( MediaType.APPLICATION_JSON )
+    @Produces( MediaType.TEXT_PLAIN )
+    public Response createAffectation(Affectation affectation) {
+        if(affectation == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        try {
+            affectationDAO.register(affectation);
+            URI uri = uriInfo.getAbsolutePathBuilder().path(Integer.toString(affectation.getId())).build();
+            return Response.created(uri).build();
+        } catch (ConstraintViolationException cve) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes( MediaType.APPLICATION_JSON )
+    @Produces( MediaType.APPLICATION_JSON )
+    public Response updateUv(Affectation affectation) {
+        if(affectation == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        try {
+            affectationDAO.update(affectation);
+            return Response.ok(affectation).build();
+        } catch(ConstraintViolationException cve) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        } catch(Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
